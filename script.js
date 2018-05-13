6468bce250b0c61d1f2670828eb962bf32a344b3
Glitch (loud-chronometer) <none>
2018-05-13 14:21:15 +0000

    Tweaks

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-;(function ({ mapboxgl, MapboxGeocoder }) {
+;(function ({ mapboxgl, MapboxGeocoder }) { 
   // Dataset
   // docs - https://dev.socrata.com/foundry/data.nashville.gov/28i3-48zr
   // portal - https://data.nashville.gov/Police/Metro-Nashville-Davidson-County-Calls-For-Police-S/kqb6-kd6q
 
-  const callPopup = new mapboxgl.Popup()
+  const callPopup = new mapboxgl.Popup()  
   const rowCount = document.getElementById('rowCount')
   rowCount.innerText = 0
-
+  
   const dayScrubber = document.getElementById('dayScrubber')
-  if (dayScrubber) {
-    dayScrubber.setAttribute('value', dayScrubber.max);
-    dayScrubber.setAttribute('disabled', true)
+  if (dayScrubber) { 
+    dayScrubber.setAttribute('value', dayScrubber.max); 
+    dayScrubber.setAttribute('disabled', true) 
   }
-
+  
   const dateSelection = document.getElementById('dateSelection')
-
+  
   mapboxgl.accessToken = 'pk.eyJ1Ijoibmljay1hdC1mb2ciLCJhIjoiY2poNDlmYzV4MGxmYTMzb2F5eXJtMmhyZSJ9.6t-4WlMIZc5yjctGUu4XVQ';
-
+  
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10',
@@ -32,7 +32,7 @@
   function popColor () {
     return colors.pop() || 'cyan'
   }
-
+  
   map.on('load', () => {
      map.addSource('geocoder-result', {
         type: 'geojson',
@@ -60,23 +60,23 @@
       })
     const socrataToken = 'ZBIG3Lx4BagIQAnFaSxdXbo2s'
     const query = `
-      SELECT
+      SELECT 
         description,
         tencode,
         tencode_suffixf_description,
         unit_dispatched,
         event_number,
-        call_received,
+        call_received, 
         date_trunc_ymd(call_received) as $call_date,
-        shift,
-        disposition_description,
-        mapped_location,
+        shift, 
+        disposition_description, 
+        mapped_location, 
         street_name,
         block,
         zone
       ORDER BY call_received DESC
       LIMIT 20000`
-
+    
     fetch(`https://data.nashville.gov/resource/28i3-48zr.json?$$app_token=${socrataToken}&$query=${query}`)
       // wild and free i just believe
       .then(resp => resp.json())
@@ -94,41 +94,41 @@
 
         const callFeatures = genFeatures(body)
         rowCount.innerText = callFeatures.length
-
+        
         const days = Object.keys(callFeatures.reduce((acc, f) => {
             acc[f.properties.$call_date] = 0
             return acc
           }, {}))
-
+        
         if (dayScrubber) {
           dayScrubber.setAttribute('max', days.length - 1)
           dayScrubber.setAttribute('value', dayScrubber.max)
-
+          
           dateSelection.innerText = days[0].split('T')[0]
-
+          
           dayScrubber.removeAttribute('disabled')
-
+   
           dayScrubber.addEventListener('input', ev => {
             let len = days.length - 1
-
+            
             const val = parseInt(ev.target.value, 10)
-
+            
             setDayFilter(map, days[len - val])
-
+            
             dateSelection.innerText = days[len - val].split('T')[0]
-
+            
             callPopup.remove()
-
+            
             console.info('filter set to ', val, days[len - val])
           })
-
+        
           console.info('dayScrubber - total days:', dayScrubber.max, 'days in result set:', days)
         }
-
+      
         console.info('data - calls for service:', callFeatures.length)
         console.info(`${descriptionToColor.length / 2} different description labels`)
         console.table(descriptionToColor.filter(l => l !== 'cyan'))
-
+      
         map.addSource('police-calls', {
           type: "geojson",
           data: {
@@ -166,8 +166,8 @@
             'text-color': 'black'
           }
         }, 'geocoder-pin')
-
-        setDayFilter(map, days[0])
+      
+        setDayFilter(map, days[0])  
 
         map.on('click', 'call-points', (ev) => {
           const call = ev.features[0]
@@ -193,8 +193,8 @@
     })
     .catch(err => console.error('error on map load', err))
   })
-
-  function setDayFilter (map, date) {
+  
+  function setDayFilter (map, date) { 
     map.setFilter('call-points', ['==', '$call_date', date])
     map.setFilter('call-labels', ['==', '$call_date', date])
   }
@@ -209,8 +209,8 @@
     const coordinates = mapped_location && mapped_location.coordinates || []
 
     const date =  new Date(policeCall.call_received)
-
-    return {
+    
+    return { 
       properties: {
         description: policeCall.description,
         tencode: policeCall.tencode,
@@ -232,8 +232,8 @@
     }
   }
 
-  function showPopup (popup, { // call object
-    geometry: { coordinates },
+  function showPopup (popup, { // call object 
+    geometry: { coordinates }, 
     properties: {
       description,
       tencode,
@@ -252,10 +252,10 @@
       .setLngLat(coordinates)
       .setHTML(`
 <div class="callInfo">
-  <h2 class="f__inline"> 10-${tencode} </h2>
+  <h2 class="f__inline"> 10-${tencode} </h2>        
   <h3 style="padding-left: 4px;" class="f__inline f__right"> ${description} </h3>
   <p> <span class="f__fade"> Unit </span> ${unit_dispatched} <span class="f__fade"> dispatched to </span> ${block} block, ${street_name} </p>
-
+  
   <table>
     <tr>
       <td class="f__fade"> ECC CAD System ID </td>
